937541eb10cd76fbaae60c4546341fd452f568f5
Quentin BERNET <q.bernet@gmail.com>
2018-03-31 22:10:31 +0200

    récup utilisateur
    récuperation de l'utilisateur et envoi de fausses informations

--- index.js
+++ index.js
@@ -24,19 +24,54 @@ app.post('/', function(req, res) {
       "message":"Votre token n'est pas valide ou est mal renseigné"
     };
   } else {
-    var idReceived = req.body.id;
-    if (idReceived === undefined || idReceived === "undefined" || idReceived === null) {
+    var idReceived = parseInt(req.body.id);
+    if (idReceived === undefined || idReceived === "undefined" || idReceived === null || isNaN(idReceived)) {
       retCode = 400;
       retData = {
         "code":retCode,
-        "message":"Un ou plusieurs paramètres sont manquants"
+        "message":"Un ou plusieurs paramètres sont manquants ou malformés"
       };
     } else {
-      retCode = 200;
-      retData = {
-        "code":retCode,
-        "message":"OK",
-      };
+      var localUsers = readJsonFileSync('users.json');
+      if (localUsers === 'undefined' || localUsers === undefined || localUsers === null || localUsers.users === 'undefined' || localUsers.users === undefined || localUsers.users === null ||(localUsers.users.length == 0)) {
+        retCode = 500 ;
+        retData = {
+          "code":retCode,
+          "message":"[Erreur technique] - la base de données utilisateurs n'est pas joignable"
+        }
+      } else {
+        localUsers = localUsers.users;
+        var currentUser = null;
+        localUsers.forEach( function (user) {
+          if (user.id === idReceived) {
+            currentUser = user;
+          }
+        });
+        if (currentUser === null) {
+          retCode = 404;
+          retData = {
+            "code":retCode,
+            "message": "[Utilisateurs manquant] - vous n'êtes pas encore réferencé dans nos bases."
+          };
+        } else {
+          retCode = 200;
+          retData = {
+            "code":retCode,
+            "message":"Bravo, voici les informations nécessaires pour la suite du test.",
+            "user":currentUser,
+            "subject": {
+              "url":"https://www.google.fr"
+            },
+            "resources": {
+              "api": {
+                "url":"https://www.google.fr",
+                "X-Auth-Token":"toto",
+                "doc":"https://www.google.fr"
+              }
+            }
+          };
+        }
+      }
     }
   }
   res.status(retCode).send(retData);
