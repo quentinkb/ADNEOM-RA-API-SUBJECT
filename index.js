45fd0e92b43e3495cca89f2a708a264aac528af8
Quentin BERNET <q.bernet@gmail.com>
2018-05-21 11:22:14 +0200

    test big serial

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const client = new Client({
 });
 
 
-var requestString = "CREATE TABLE access (id int IDENTITY(1,1) PRIMARY KEY, id_user INT NOT NULL, last_name varchar(255), first_name varchar(255), access_time DATE, success int, code varchar(255), message varchar(255)); ";
+var requestString = "CREATE TABLE access (id BIGSERIAL PRIMARY KEY, id_user INT NOT NULL, last_name varchar(255), first_name varchar(255), access_time DATE, success int, code varchar(255), message varchar(255)); ";
 
 client.connect().catch(function (result){
   console.log(result);
